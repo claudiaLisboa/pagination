1141a5513ef1410c6f5c0fa59edfa4c67517426b
Claudia Lisboa <ccalumby@gmail.com>
2023-01-04 14:29:58 -0500

    added useEffect, useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export default function App() {
+
+   const [products,setProducts] = useState([])
+
+   const fetchProducts = async ()=>{
       
-    </div>
-  );
+      const res = await fetch(`https://dummyjson.com/products?limit=100`);
+      const data = await res.json();
+
+      if (data && data.products){
+          setProducts(data.products);
+      }
+   };
+    
+    //console.log(data);
+    console.log(products);
+
+    useEffect(() =>{
+      fetchProducts()
+
+    },[])
+    return (
+      <div >
+           {
+              products.length > 0 && <div className="products">
+                {
+                  products.map((prod) => {
+                    return <span>{prod.title}</span>
+
+                  })
+                }
+
+              </div>
+           }
+        
+      </div>
+    );
 }
 
-export default App;
+
